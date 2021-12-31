f312171371656c55800cfe9a10aab98f507f775e
dingsheng <727040141@qq.com>
2021-12-31 17:39:53 +0800

    feat: add loadJs objToUrl urlToObj

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export { default as debounce } from './debounce';
 export { default as throttle } from './throttle';
 export { default as uuid } from './uuid';
 export { default as deepcopy } from './deepcopy';
+export { default as loadJS } from './loadJs';
+export { default as objToUrl } from './objToUrl';
+export { default as urlToObj } from './urlToObj';
